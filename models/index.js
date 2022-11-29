06ac8cdbe12b7300ed7fd99c2f64b76ce35bca23
rifalghurofan <rifalghurofan@gmail.com>
2022-11-29 08:47:00 +0700

    fixing

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.Promise = global.Promise;
-
-const db = mongoose.connect(process.env.MONGODB_URI);
-db.mongoose = mongoose;
-db.Promotions = require("./promotions");
-module.exports = db;
